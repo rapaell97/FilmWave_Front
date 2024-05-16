e2123b799c481be416f0c295dc65a96ee62aabc2
park <rapaell97@naver.com>
2024-05-16 12:18:23 +0900

    refact headNavbar opacity

--- src/components/HeadNavbar.vue
+++ src/components/HeadNavbar.vue
@@ -1,37 +1,60 @@
 <template>
-    <nav>
+    <nav :style="{backgroundColor: navBackgroundColor}">
         <RouterLink :to="{ name: 'home' }">
             <img src="@/assets/logo.png" alt="#" class="logo-icon">
         </RouterLink>
-        <div class="left-nav">
-            <RouterLink :to="{ name: 'login' }">로그인</RouterLink>
+        <div class="right-nav">
+            <RouterLink v-if="usestore.isLogin" :to="{ name: 'login' }">로그인</RouterLink>
+            <button @click="logout">로그아웃</button>
             <RouterLink :to="{ name: 'mypage' }">Mypage</RouterLink>
+
             <img src="@/assets/search.png" alt="" class="icon">
-            <input type="text">
         </div>
     </nav>
 </template>
 
 <script setup>
+import { ref, onMounted, computed } from 'vue'
 import { RouterLink } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 
-const store = useUserStore()
+const usestore = useUserStore()
+const nowScroll = ref(0)
+
+const updateScroll = function() {
+    nowScroll.value = window.scrollY
+    console.log(nowScroll.value)
+}
+
+const navBackgroundColor = computed(() => {
+    const opacity = nowScroll.value > 10 ? 1 : 0.3
+    return `rgba(0, 0, 128, ${opacity})` // navy 색상의 RGBA 표현
+})
+
+onMounted(() => {
+    window.addEventListener('scroll', updateScroll)
+})
+
+const logout = function() {
+    usestore.isLogin = false
+}
 
 </script>
 
 <style scoped>
 nav{
+    position: sticky;
+    top: 0;
     display: flex;
     justify-content: space-between;
     align-items: center;
     background-color: navy;
+    width: 100%;
     height: 60px;
-    padding-left: 20px;
-    padding-right: 20px;
+    transition: background-color 0.3s ease-in-out;
 }
 
-.left-nav{
+.right-nav{
     display: flex;
     align-items: center;
 }
@@ -45,6 +68,5 @@ nav{
     width: 30px;
     height: 30px;
 }
-
 
 </style>
