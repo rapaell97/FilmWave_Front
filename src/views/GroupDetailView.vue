1f96d92dbaf8448d1e536e11d23067f81b4d4095
minhyeok <rapaell97@naver.com>
2024-05-19 14:09:15 +0900

    feat-GroupMangement signup request

--- src/views/GroupDetailView.vue
+++ src/views/GroupDetailView.vue
@@ -1,5 +1,11 @@
 <template>
     <h1>그룹상세페이지</h1>
+    <div v-if="isModalOpen" class="modal-overlay" @click="closeModal">
+      <div class="modal-content" @click.stop>
+        <p>가입 요청이 성공적으로 전송되었습니다!</p>
+        <button @click="closeModal">닫기</button>
+      </div>
+    </div>
     <div class="group-info">
         <img src="@/assets/group2.png" alt="" class="group-img">
         <div>
@@ -16,6 +22,11 @@
       그룹 삭제</button>
     </div>
     <RouterLink :to="{name:'groupManageMent', params:{'groupId': groupId }}">그룹 관리</RouterLink>
+    <button
+    v-if="isMember === false"
+    @click="groupSignupRequest"
+    >
+    가입하기</button>
 </template>
 
 <script setup>
@@ -26,7 +37,6 @@ import { useGroupStore } from '@/stores/group'
 import { useUserStore } from '@/stores/user'
 import { formatDate } from '@/utils/datefomatter'
 
-
 const route = useRoute()
 const router = useRouter()
 const groupstore = useGroupStore()
@@ -34,6 +44,8 @@ const userstore = useUserStore()
 
 const groupId = ref(route.params.groupId)
 const group = ref('')
+const isMember = ref(false)
+const isModalOpen = ref(false)
 
 const groupDelete = function(){
   axios({
@@ -61,13 +73,45 @@ onMounted(() => {
     //   console.log(response.data)
       group.value = response.data
       console.log(group.value)
+      isMemberCheck()
     })
     .catch(error => {
       console.log(error)
     })
 })
 
+const isMemberCheck = function(){
+  group.value.members.forEach((member) => {
+    if (member.id === userstore.userId){
+      isMember.value = true
+    }
+  })
+}
 
+const openModal = () => {
+  isModalOpen.value = true
+}
+
+const closeModal = () => {
+  isModalOpen.value = false
+}
+
+const groupSignupRequest = function() {
+  axios({
+    method: 'POST',
+    url: `${groupstore.API_URL}/groups/${groupId.value}/membership-requests/`,
+    headers:{
+        Authorization: `Token ${userstore.token}`
+      }
+  })
+  .then(response => {
+    console.log(response.data)
+    openModal()
+  })
+  .catch(error => {
+    console.log(error)
+  })
+}
 
 </script>
 
@@ -80,6 +124,24 @@ onMounted(() => {
 }
 .group-img{
     width: 150px;
+}
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 300px;
+  text-align: center;
 }
 </style>
