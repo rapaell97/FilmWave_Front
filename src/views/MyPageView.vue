818488d8104b71ef2328645531306f1f775cc3ab
JaewonChoi <wefhio1985@gmail.com>
2024-05-22 20:28:38 +0900

    feat-내 그룹 목록

--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -3,10 +3,12 @@
         <h1>마이 페이지</h1>
     </div>
     <LikeMovieList />
+    <MyGroupList />
 </template>
 
 <script setup>
 import LikeMovieList from '@/components/mypage/LikeMovieList.vue';
+import MyGroupList from '@/components/mypage/MyGroupList.vue';
 
 </script>
 
