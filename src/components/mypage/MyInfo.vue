0092508a1c81ca832e374d3607f8af13c411cd75
Jaewon <wefhio1985@gmail.com>
2024-05-22 17:30:20 +0900

    feat-프로필/장르 빈도 그래프

---
+++ src/components/mypage/MyInfo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        username: {{ store.userName }}
+    </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/user';
+
+const store = useUserStore()
+</script>
+
+<style lang="scss" scoped>
+
+</style>
