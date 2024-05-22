cd8215b1612d713f283feaa289ddc09bd0ac3240
park <rapaell97@naver.com>
2024-05-22 17:43:35 +0900

    feat-GroupDetail GroupBoard

--- src/components/group/GroupPostArticle.vue
+++ src/components/group/GroupPostArticle.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>{{ post.id }}. {{ post.title }}</h3>
+    <h3 style="cursor: pointer;">{{ post.id }}. {{ post.title }}</h3>
     <p><i class="fa-solid fa-comment" style="color: #36383a;"></i> {{ post.comment_set.length }}개 댓글</p>
     <hr>
   </div>
