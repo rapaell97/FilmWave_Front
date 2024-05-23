1d4c0d0f4f466d486c17898f287f8355d4be5882
Jaewon <wefhio1985@gmail.com>
2024-05-23 10:52:41 +0900

    feat-사용자 리뷰목록 조회

--- src/components/mypage/MyReviewList.vue
+++ src/components/mypage/MyReviewList.vue
@@ -15,7 +15,10 @@ import { onMounted, ref } from 'vue';
 import MovieWithReviewCard from '../MovieWithReviewCard.vue';
 import { useUserStore } from '@/stores/user';
 import axios from 'axios';
+import { useRoute } from 'vue-router';
 
+
+const route = useRoute()
 const userStore = useUserStore()
 const reviews = ref([])
 
@@ -23,10 +26,7 @@ const reviews = ref([])
 onMounted(() => {
   axios({
     method: 'GET',
-    url: `${userStore.API_URL}/movies/reviews/my/`,
-    headers: {
-      Authorization: `Token ${userStore.token}`
-    }
+    url: `${userStore.API_URL}/movies/${route.params.username}/reviews/`,
   })
   .then(res => {
     console.log(res.data)
