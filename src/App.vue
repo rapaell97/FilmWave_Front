fec43c5b7b63e2bbec642a03b90a08116d39e03e
minhyeok <rapaell97@naver.com>
2024-05-24 04:10:56 +0900

    refeat-MovieDetail scroll

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,33 @@ html, body {
   background-image: url('@/assets/background.png');
 }
 
+/* 스크롤바 스타일 */
+::-webkit-scrollbar {
+  width: 1px;
+}
+
+::-webkit-scrollbar-track {
+  background: #2e2e2e;
+}
+
+::-webkit-scrollbar-thumb {
+  background-color: #555;
+  border-radius: 10px;
+  border: 2px solid #2e2e2e;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #777;
+}
+
+body {
+  overflow: overlay;
+}
+
+html {
+  scrollbar-width: thin;
+  scrollbar-color: #555 #2e2e2e;
+}
+
 </style>
+
